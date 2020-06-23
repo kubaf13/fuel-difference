a2b63b3b48f729548901a089fe2021a26fcf1139
kubaf13 <kubaf13@gmail.com>
2020-06-24 00:31:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const cityMileage = document.getElementById("cityMileage");
+const motorwayMileage = document.getElementById("motorwayMileage");
+const costAdvantage = document.getElementById("cost-advantage");
+const car = document.getElementById("car");
+const fuelPrice = 5;
+const fuelConsumption = {
+  petrol: {
+    city: 5.6 / 100,
+    motorway: 4.6 / 100,
+  },
+  hybrid: {
+    city: 5.4 / 100,
+    motorway: 5 / 100,
+  },
+};
+let fuelCost = {
+  petrol: "",
+  hybrid: "",
+};
+
+function showFuelCost() {
+  fuelCost.petrol = Math.round(
+    (cityMileage.value * fuelConsumption.petrol.city +
+      motorwayMileage.value * fuelConsumption.petrol.motorway) *
+      fuelPrice
+  );
+  fuelCost.hybrid = Math.round(
+    (cityMileage.value * fuelConsumption.hybrid.city +
+      motorwayMileage.value * fuelConsumption.hybrid.motorway) *
+      fuelPrice
+  );
+
+  if (fuelCost.petrol > fuelCost.hybrid) {
+    costAdvantage.innerHTML = fuelCost.petrol - fuelCost.hybrid + " zł";
+    car.innerHTML = "Hybrid";
+  } else if (fuelCost.petrol < fuelCost.hybrid) {
+    costAdvantage.innerHTML = fuelCost.hybrid - fuelCost.petrol + " zł";
+    car.innerHTML = "Petrol";
+  } else {
+    costAdvantage.innerHTML = "0 zł";
+    car.innerHTML = "";
+  }
+}
+
+cityMileage.addEventListener("change", showFuelCost);
+motorwayMileage.addEventListener("change", showFuelCost);
